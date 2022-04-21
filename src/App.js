f2fdce3a407c89a20dd5fbb127f48f20e1bdb53f
Asther Marie Moreno <ashterfuentes@gmail.com>
2022-04-21 13:47:19 -0500

    added a link to my github repository

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ export default function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <SearchEngine />
+
+      <small>
+        <a href="https://github.com/AshM10/weather-app-react" target="_blank">
+          Open-source
+        </a>{" "}
+        code, by Ash Moreno
+      </small>
     </div>
   );
 }
